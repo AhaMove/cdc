c0fcd4b1a19bd2605d801369ea7fb7f281e01a95
Nguyen Ngoc Hung <hungnn@ahamove.com>
2020-11-23 16:22:24 +0700

    remove run project

--- run/docker/monitor_supplier_location.sh
+++
@@ -1,4 +0,0 @@
-if [ $(docker inspect -f '{{.State.Running}}' supplier_location) = "false" ]; then
-  curl -s -O "https://admin.ahamove.com/public/v1/health/alert?target=supplier_location_ahamove_staging&email=thuc@ahamove.com,hungnn@ahamove.com";
-  docker restart supplier_location
-fi
--- run/supplier_location.sh
+++
@@ -1,6 +0,0 @@
-POSTGRES_URL="postgresql://doadmin:i6g87966roxc074p@aha-staging-do-user-1538085-0.db.ondigitalocean.com:25060/ahamove?sslmode=require" \ 
-MONGO_URL="mongodb+srv://admin:Aha2020@ahamove-sz8j3.mongodb.net/ahamove" \
-nohup moresql --config-file=/mnt/volume_airflow3/cdc/bin/supplier_location.json --tail=true \ 
---checkpoint=true -app-name=supplier_location_csv --exports=csv --tail-type=change-stream \
---csv-path-file=/mnt/volume_airflow3/archive/gs/supplier_location_moresql/supplier_location.csv \ 
-> /mnt/volume_airflow3/archive/gs/supplier_location_moresql/supplier_location.out 2>&1 &
